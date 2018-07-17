d787e2815c8f7741df1a7cac4c8dd2e3a0fb844d
Philipp Schlegel <observing@web.de>
2018-07-17 10:42:23 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -27,24 +27,25 @@ setup(
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        
+
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
 
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        
+
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    ],    
+    ],
 
-    install_requires=[        
-        "scipy>=0.18.1",        
+    install_requires=[
+        "scipy>=0.18.1",
         "numpy>=1.12.1",
-        "matplotlib>=2.0.0",     
-        "tqdm"   
+        "pandas",
+        "matplotlib>=2.0.0",
+        "tqdm"
     ],
 
     python_requires='>=3',
